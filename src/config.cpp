21f5a5c343e2589e1723aa92d67fbc760460953c
Andrey Vasilyev <sagegeek@gmail.com>
2019-05-07 20:30:44 -0500

    Level & Type are strings in Json

--- src/config.cpp
+++ src/config.cpp
@@ -17,15 +17,18 @@ void Config::load(String name)
     {
         JsonObject &root = jsonBuffer.parseObject(file);
         file.close();
-        if (!root.success()) {
+        if (!root.success())
+        {
             Serial.println("JSON parse error");
         }
         else
         {
             configFromJson(root);
             Serial.println("config loaded");
+            return;
         }
     }
+    loadDefault();
 }
 
 void Config::save()
@@ -85,9 +88,9 @@ JsonObject &Config::configToJson(JsonBuffer &doc)
     {
         JsonObject &ch = array2.createNestedObject();
         ch["channel"] = dmx[i].channel;
-        ch["type"] = (int)dmx[i].type;
+        ch["type"] = dmxTypeToString(dmx[i].type);
         ch["pin"] = dmx[i].pin;
-        ch["level"] = dmx[i].level;
+        ch["level"] = dmx[i].level ? "HIGH" : "LOW";
         ch["pulse"] = dmx[i].pulse;
         ch["multiplier"] = dmx[i].multiplier;
         ch["threshold"] = dmx[i].threshold;
@@ -97,20 +100,22 @@ JsonObject &Config::configToJson(JsonBuffer &doc)
 
 bool Config::configFromJson(JsonObject &object)
 {
+    cleanup();
     Serial.println("Parsing config...");
     // Networks
-    host = object.get<String>("host");
+    if (object["host"].is<char *>())
+    {
+        host = object.get<String>("host");
+    }
+    else
+    {
+        Serial.println("`Host` is not a string");
+        return false;
+    }
     Serial.println("Host:" + host);
     JsonArray &nets = object["wifi"];
-    for (WiFiNet net : wifi)
-    {
-        net.ssid = "";
-        net.pass = "";
-        net.dhcp = false;
-        net.order = 0;
-    }
     Serial.println("Networks found:" + String(nets.size(), DEC));
-    wifiCount = 0;
+
     for (JsonObject &net : nets)
     {
         if (wifiCount < MAX_NETWORKS)
@@ -131,6 +136,41 @@ bool Config::configFromJson(JsonObject &object)
 
     // DMX
     JsonArray &channels = object["dmx"];
+
+    Serial.println("DMX found:" + String(channels.size(), DEC));
+    for (JsonObject &channel : channels)
+    {
+        if (dmxCount < MAX_DMX_CHANNELS && channel["type"] > 0)
+        {
+            dmx[dmxCount].type = dmxTypeFromString(channel.get<String>("type"));
+            dmx[dmxCount].channel = channel.get<uint8_t>("channel");
+            dmx[dmxCount].threshold = channel.get<uint8_t>("threshold");
+            dmx[dmxCount].pin = channel.get<uint8_t>("pin");
+            dmx[dmxCount].level = channel.get<String>("level").equals("HIGH") ? HIGH : LOW; // only the least bit counts
+            dmx[dmxCount].pulse = channel.get<uint16_t>("pulse");
+            dmx[dmxCount].multiplier = channel.get<uint16_t>("multiplier");
+            Serial.println("DMX[" + String(dmxCount, DEC) + "]: " + String(dmx[dmxCount].type, DEC) + " " + String(dmx[dmxCount].channel, DEC));
+            dmxCount++;
+        }
+    }
+    // HW
+    JsonObject &hw = object["hw"];
+    hardware.pwmFreq = hw.get<uint16_t>("freq");
+    return host && (dmxCount + wifiCount) > 0;
+}
+
+void Config::cleanup()
+{
+    host = DEFAULT_HOST_NAME;
+    hardware.pwmFreq = DEFAULT_PWM_FREQ;
+    for (WiFiNet net : wifi)
+    {
+        net.ssid = "";
+        net.pass = "";
+        net.dhcp = false;
+        net.order = 0;
+    }
+    wifiCount = 0;
     for (DmxChannel channel : dmx)
     {
         channel.channel = 0;
@@ -142,24 +182,22 @@ bool Config::configFromJson(JsonObject &object)
         channel.multiplier = 0;
     }
     dmxCount = 0;
-    Serial.println("DMX found:" + String(channels.size(), DEC));
-    for (JsonObject &channel : channels)
-    {
-        if (dmxCount < MAX_DMX_CHANNELS && channel["type"] > 0)
-        {
-            dmx[dmxCount].type = static_cast<DmxType>(channel.get<int>("type"));
-            dmx[dmxCount].channel = channel.get<uint8_t>("channel");
-            dmx[dmxCount].threshold = channel.get<uint8_t>("threshold");
-            dmx[dmxCount].pin = channel.get<uint8_t>("pin");
-            dmx[dmxCount].level = channel.get<uint8_t>("level");    // only the least bit counts
-            dmx[dmxCount].pulse = channel.get<uint16_t>("pulse");
-            dmx[dmxCount].multiplier = channel.get<uint16_t>("multiplier");
-            Serial.println("DMX[" + String(dmxCount, DEC) + "]: " + String(dmx[dmxCount].type, DEC) + " " + String(dmx[dmxCount].channel, DEC));
-            dmxCount++;
-        }
-    }
-    // HW
-    JsonObject &hw = object["hw"];
-    hardware.pwmFreq = hw.get<uint16_t>("freq");
-    return host && (dmxCount + wifiCount) > 0;
+}
+
+/**
+ * Here can go default configurations for different boards 
+*/
+void Config::loadDefault()
+{
+    Serial.println("Loading default configuration");
+    cleanup();
+    dmx[dmxCount].type = DmxType::Dimmable;
+    dmx[dmxCount].channel = DEFAULT_DMX_CHANNEL;
+    dmx[dmxCount].threshold = 0;
+    dmx[dmxCount].pin = LED_BUILTIN;
+    dmx[dmxCount].level = LOW; // only the least bit counts
+    dmx[dmxCount].pulse = DEFAULT_DMX_PULSE;
+    dmx[dmxCount].multiplier = DEFAULT_DMX_MULIPLIER;
+    Serial.println("DMX[" + String(dmxCount, DEC) + "]: " + String(dmx[dmxCount].type, DEC) + " " + String(dmx[dmxCount].channel, DEC));
+    dmxCount++;
 }
--- src/config.h
+++ src/config.h
@@ -8,66 +8,103 @@ extern "C"
 }
 #endif
 
+#define DEFAULT_PWM_FREQ 600
+#define DEFAULT_HOST_NAME "ArtNetNode"
+
+#define DEFAULT_DMX_CHANNEL 1
+#define DEFAULT_DMX_PULSE 20
+#define DEFAULT_DMX_MULIPLIER 5
+
 typedef struct
 {
-    String ssid;
-    String pass;
-    bool dhcp;
-    uint8_t order;
+  String ssid;
+  String pass;
+  bool dhcp;
+  uint8_t order;
 } WiFiNet;
 #define MAX_NETWORKS 8
 
-enum DmxType { Disabled = 0, Binary = 1, Dimmable = 2 };
+enum DmxType
+{
+  Disabled = 0,
+  Binary = 1,
+  Dimmable = 2
+};
 
 typedef struct
 {
-    // DMX channel
-    uint8_t channel;
-    // DMX channel features
-    DmxType type;
-    // ON/OFF threshold for a DmxType::Binary switch (relay)
-    uint8_t threshold;
-    // Strobe pulse length
-    uint16_t pulse;
-    // Strobe period multiplier
-    uint16_t multiplier;
-    // Pin
-    uint8_t pin;
-    // Active level: 0=LOW, 1=HIGH
-    uint8_t level;
+  // DMX channel
+  uint8_t channel;
+  // DMX channel features
+  DmxType type;
+  // ON/OFF threshold for a DmxType::Binary switch (relay)
+  uint8_t threshold;
+  // Strobe pulse length
+  uint16_t pulse;
+  // Strobe period multiplier
+  uint16_t multiplier;
+  // Pin
+  uint8_t pin;
+  // Active level: 0=LOW, 1=HIGH
+  uint8_t level;
 } DmxChannel;
 #define MAX_DMX_CHANNELS 4
 
 typedef struct
 {
   // PWM frequency
-  uint16_t  pwmFreq = 200;  // ESP8266 default is 1000 which may cause MOSFER overheat
+  uint16_t pwmFreq = DEFAULT_PWM_FREQ; // ESP8266 default is 1000 which may cause MOSFER overheat
 } HardwareConfig;
 
 class Config
 {
-    const int JSON_SIZE = 1024;
-    String fileName;
-    //StaticJsonBuffer<2048> jsonBuffer;
-    DynamicJsonBuffer jsonBuffer;
-  protected:
-    JsonObject& configToJson(JsonBuffer &doc);
-    bool configFromJson(JsonObject& object);
-    int wifiCount;
-    int dmxCount;
-  public:
-    // DMX
-    DmxChannel dmx[MAX_DMX_CHANNELS];
-    // WiFi
-    WiFiNet wifi[MAX_NETWORKS];
-    String host;
-    HardwareConfig hardware;
+  const int JSON_SIZE = 1024;
+  String fileName;
+  DynamicJsonBuffer jsonBuffer;
 
-    Config();
+protected:
+  const String DMX_DISABLED = String("DISABLED");
+  const String DMX_BINARY = String("BINARY");
+  const String DMX_DIMMABLE = String("DIMMABLE");
 
-    void load(String name);
-    void save();
-    void serialize(String &to);
-    bool deserialize(String from);
-    bool deserialize(JsonObject& object);
+  JsonObject &configToJson(JsonBuffer &doc);
+  bool configFromJson(JsonObject &object);
+  void loadDefault();
+  void cleanup();
+  int wifiCount;
+  int dmxCount;
+
+  DmxType dmxTypeFromString(String type)
+  {
+    if (DMX_BINARY.equals(type))
+      return DmxType::Binary;
+    if (DMX_DIMMABLE.equals(type))
+      return DmxType::Dimmable;
+    return DmxType::Disabled;
+  }
+
+  String dmxTypeToString(DmxType type)
+  {
+    if (type == DmxType::Binary)
+      return DMX_BINARY;
+    if (type == DmxType::Dimmable)
+      return DMX_DIMMABLE;
+    return DMX_DISABLED;
+  }
+
+public:
+  // DMX
+  DmxChannel dmx[MAX_DMX_CHANNELS];
+  // WiFi
+  WiFiNet wifi[MAX_NETWORKS];
+  String host;
+  HardwareConfig hardware;
+
+  Config();
+
+  void load(String name);
+  void save();
+  void serialize(String &to);
+  bool deserialize(String from);
+  bool deserialize(JsonObject &object);
 };
